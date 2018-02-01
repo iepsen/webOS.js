31fa5cd444a719c3ffa48d9aefd43c6f68cb6e10
Marcelo Iepsen <miepsen@horizonte.tv.br>
2018-02-01 19:39:13 -0200

    exporting modules

---
+++ index.js
@@ -0,0 +1,11 @@
+import 'src/core/foundation';
+import 'src/core/platform';
+
+import 'src/application';
+import 'src/deviceinfo';
+import 'src/feedback';
+import 'src/keyboard';
+import 'src/notification';
+import 'src/service';
+import 'src/version';
+import 'src/voicereadout';
