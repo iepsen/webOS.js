b1b5224cdeffdc3379dbb979eba93ced62f9adc3
Marcelo Iepsen <miepsen@horizonte.tv.br>
2018-02-02 12:53:07 -0200

    fixing import

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import 'src/core/foundation';
-import 'src/core/platform';
-
-import 'src/application';
-import 'src/deviceinfo';
-import 'src/feedback';
-import 'src/keyboard';
-import 'src/notification';
-import 'src/service';
-import 'src/version';
-import 'src/voicereadout';
+import './src/core/foundation';
+import './src/core/platform';
+import './src/application';
+import './src/deviceinfo';
+import './src/feedback';
+import './src/keyboard';
+import './src/notification';
+import './src/service';
+import './src/version';
+import './src/voicereadout';
